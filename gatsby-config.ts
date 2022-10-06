fc36eb65a05d4a58783364b27711eb97a2f50a7a
Shuqian Hon <honsq90@gmail.com>
2022-10-06 12:24:04 +1100

    Add gatsby-transformer-sharp to remove typegen errors

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,7 +10,8 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-    'gatsby-plugin-postcss',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-postcss`,
   ],
 }
 

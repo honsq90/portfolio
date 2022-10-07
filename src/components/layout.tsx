4318c528be47445e36d0bf8a115c2eaaf194a450
Shuqian Hon <honsq90@gmail.com>
2022-10-07 23:34:57 +1100

    Change layout and queries for easier management

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,26 +1,27 @@
 import { graphql, PageProps } from "gatsby";
 import React from "react";
 import Helmet from "react-helmet";
+import logo from "../static/images/logo.png";
 
 export default function Layout({ data, children }: PageProps<Queries.IndexPageQuery>) {
 
   return (
-    <>
+    <div className="flex">
       <Helmet>
         <title>{data?.site?.siteMetadata?.title}</title>
         <meta name="siteUrl" content={data?.site?.siteMetadata?.siteUrl} />
-        <body className="bg-slate-900 text-slate-300 overflow-hidden w-full h-screen bg-gradient-to-tr from-slate-900 to-slate-700 bg-no-repeat" />
+        <body className="bg-slate-900 overflow-hidden w-full h-screen bg-gradient-to-tr from-slate-900 to-slate-700 bg-no-repeat" />
       </Helmet>
-      <main className="opacity-100">
-        <div className="w-full h-12 text-center text-3xl pt-1">
+      <header className="flex-none w-1/4 h-screen bg-white flex flex-col ">
+        <img src={logo} alt="Site logo" className="flex-none w-16 h-auto justify-center self-center  py-4"/>
+        <div className="text-center text-3xl pt-1">
           Shuqian Hon Photography
         </div>
-        <section>
-          {children}
-        </section>
+      </header>
+      <main className="flex-auto">
+        {children}
       </main>
-    </>
-
+    </div>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,10 @@ import * as React from "react";
 
 const IndexPage = ({ data }: PageProps<Queries.AllPhotosQuery>) => {
   return (
-    <div className="grid grid-flow-row grid-cols-4 gap-2">
-      {data.allContentfulPhoto.nodes.map((node) => <div className="relative cursor-pointer">
-        <GatsbyImage image={node.image?.gatsbyImageData!} alt=""/>
-        <span className="opacity-0 hover:opacity-100 hover:bg-black hover:bg-opacity-40 transition-opacity ease-in-out duration-500 absolute left-0 right-0 top-0 bottom-0 flex flex-col justify-end text-center">{node.name}</span>
+    <div className="grid grid-flow-row grid-cols-2 overflow-scroll max-h-screen">
+      {data.allContentfulAsset.nodes.map((node) => <div className="relative cursor-pointer">
+        <GatsbyImage image={node.gatsbyImageData!} alt="" />
+        <span className="opacity-0 hover:opacity-100 hover:bg-black hover:bg-opacity-40 transition-opacity ease-in-out duration-500 absolute left-0 right-0 top-0 bottom-0 flex flex-col justify-end text-center text-slate-300">{node.title}</span>
       </div>)}
     </div>
   );
@@ -17,14 +17,11 @@ export default IndexPage;
 
 export const query = graphql`
  query AllPhotos {
-  allContentfulPhoto {
+  allContentfulAsset {
     nodes {
       id
-      name
-      dateTaken
-      image {
-        gatsbyImageData(height: 800)
-      }
+      title
+      gatsbyImageData(height: 800)
     }
   }
  }

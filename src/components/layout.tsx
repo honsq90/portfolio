d769df9421e75a245f8fffe22faa65927418641d
Shuqian Hon <honsq90@gmail.com>
2022-10-15 00:10:20 +1100

    Fix padding in header

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,33 +8,37 @@ export default function Layout({
   data,
   children,
 }: PageProps<Queries.IndexPageQuery>) {
-  const years = orderBy(range(2010, 2023), [], "desc")
+  const years = orderBy(range(2010, 2023), [], "desc");
 
   return (
-
     <div className="flex flex-col overflow-scroll max-h-screen lg:max-w-none lg:flex-row">
       <Helmet>
         <title>{data?.site?.siteMetadata?.title}</title>
         <meta name="siteUrl" content={data?.site?.siteMetadata?.siteUrl} />
         <body className="bg-slate-900 overflow-hidden w-full h-screen bg-gradient-to-tr from-slate-900 to-slate-700 bg-no-repeat" />
       </Helmet>
-      <header className="fixed top-0 z-10 w-full bg-white flex-none flex flex-row px-4 md:justify-center lg:sticky lg:w-1/6 lg:h-screen lg:justify-start lg:flex-col">
+      <header className="fixed top-0 z-10 w-full bg-white flex-none flex flex-row px-4 py-1 md:justify-center lg:sticky lg:w-1/6 lg:h-screen lg:justify-start lg:flex-col">
         <img
           src={logo}
           alt="Site logo"
-          className="w-auto lg:h-24 h-10 justify-center self-center p-1 lg:py-4"
+          className="w-auto lg:h-24 h-10 justify-center self-center lg:py-4"
         />
 
-        <div className="hidden lg:block text-center text-2xl ml-1 pt-1 self-center">
+        <div className="hidden lg:block text-center text-2xl self-center">
           Shuqian Hon Photography
         </div>
 
-        <nav className="flex p-1 overflow-x-scroll w-full lg:flex lg:flex-wrap lg:pl-4 lg:pt-4 gap-1">
+        <nav className="flex gap-1 p-1 overflow-x-scroll w-full lg:flex lg:flex-wrap lg:py-8 ">
           {years.map((year) => {
-            return <a key={year} href={`#year-${year}`}
-              className="select-none flex-grow text-center p-1 border-x border-y border-transparent border-solid hover:border-b-slate-700 hover:bg-slate-200 lg:border-slate-700 lg:rounded-md lg:border lg:w-1/3 ">
-              {year}
-            </a>
+            return (
+              <a
+                key={year}
+                href={`#year-${year}`}
+                className="select-none flex-grow text-center p-1 border-x border-y border-transparent border-solid hover:border-b-slate-700 hover:bg-slate-200 lg:border-slate-700 lg:rounded-md lg:border lg:w-1/3 "
+              >
+                {year}
+              </a>
+            );
           })}
         </nav>
       </header>
@@ -42,7 +46,7 @@ export default function Layout({
         {children}
       </main>
 
-      <footer className="fixed z-10 text-xs bg-white w-full bottom-0 lg:text-base lg:left-0 lg:justify-self-end lg:w-1/6 text-center">
+      <footer className="fixed z-10 px-4 text-xs text-center bg-white w-full bottom-0 lg:text-base lg:left-0 lg:justify-self-end lg:w-1/6">
         Copyright © 2004-2022 Shuqian Hon
       </footer>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,23 +4,25 @@ import { groupBy, orderBy, reverse, sortBy } from "lodash";
 import * as React from "react";
 
 const IndexPage = ({ data }: PageProps<Queries.AllPhotosQuery>) => {
-  const photos = data.allContentfulAsset?.nodes || []
+  const photos = data.allContentfulAsset?.nodes || [];
 
-  const photosByYear = groupBy(photos, (node) => node.title!.slice(0, 4))
+  const photosByYear = groupBy(photos, (node) => node.title!.slice(0, 4));
 
   return (
     <div className="flex flex-wrap flex-grow">
-      {reverse(Object.keys(photosByYear))
-        .map((year) =>
-          <React.Fragment key={year}>
-            {photosByYear[year].map((node, index) => (
-              <div key={node.id} id={index == 0 ?`year-${year}`: undefined} className="md:w-1/2 content-center">
-                <GatsbyImage image={node.gatsbyImageData!} alt="" />
-              </div>
-            ))}
-          </React.Fragment>
-        )}
-
+      {reverse(Object.keys(photosByYear)).map((year) => (
+        <React.Fragment key={year}>
+          {photosByYear[year].map((node, index) => (
+            <div
+              key={node.id}
+              id={index == 0 ? `year-${year}` : undefined}
+              className="md:w-1/2 content-center"
+            >
+              <GatsbyImage image={node.gatsbyImageData!} alt="" />
+            </div>
+          ))}
+        </React.Fragment>
+      ))}
     </div>
   );
 };
@@ -29,7 +31,7 @@ export default IndexPage;
 
 export const query = graphql`
   query AllPhotos {
-    allContentfulAsset(sort: {fields: title, order: DESC }) {
+    allContentfulAsset(sort: { fields: title, order: DESC }) {
       nodes {
         id
         title

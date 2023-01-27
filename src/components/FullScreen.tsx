10fc7a912a35ceec614f6ef3a8baf6aabefaa640
Shuqian Hon <honsq90@gmail.com>
2023-01-27 16:04:45 +0800

    Try position: absolute instead

--- src/components/FullScreen.tsx
+++ src/components/FullScreen.tsx
@@ -50,15 +50,15 @@ export function FullScreen() {
     ariaHideApp={false}
   >
     <GatsbyImage image={selectedImage.fullScreen} alt={selectedImage.title} objectFit="contain" />
-    <button type="button" className={`text-white p-2 fixed left-0 right-1/2 md:right-3/4 top-10 bottom-0 focus:border-0 ${devMode && "border-2 border-red-700"}`}
+    <button type="button" className={`text-white p-2 absolute left-0 right-1/2 md:right-3/4 top-10 bottom-0 focus:border-0 ${devMode && "border-2 border-red-700"}`}
       onClick={() => window.postMessage({ type: "image-left" })} title="Prev">
       &nbsp;
     </button>
-    <button type="button" className={`text-white p-2 fixed right-0 left-1/2 md:left-3/4 top-10 bottom-0 focus:border-0 ${devMode && "border-2 border-red-700"}`}
+    <button type="button" className={`text-white p-2 absolute right-0 left-1/2 md:left-3/4 top-10 bottom-0 focus:border-0 ${devMode && "border-2 border-red-700"}`}
       onClick={() => window.postMessage({ type: "image-right" })} title="Next">
       &nbsp;
     </button>
-    <button type="button" className="text-white p-2 text-2xl fixed right-0" onClick={closeModal}>
+    <button type="button" className="text-white p-2 text-2xl absolute right-0" onClick={closeModal}>
       &#9587;
     </button>
   </Modal>
